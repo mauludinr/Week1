0fc67c64ec123d786275211c955b9cc736a47047
M Mauludin Rohman <38213112+mauludinr@users.noreply.github.com>
2021-10-31 19:47:53 +0700

    Create dag_2.py

---
+++ dags/dag_2.py
@@ -0,0 +1,23 @@
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.mysql_operator import MySqlOperator
+from datetime import datetime, timedelta
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2021,10,30),
+    'retries': 0,
+}
+
+dag = DAG(dag_id='DAG-2',
+          default_args=default_args,
+          catchup=False,
+          schedule_interval='@once')
+
+mysql_extract = MySqlOperator(
+    dag=dag,
+    mysql_conn_id='airflow_mysql',
+    task_id='mysql_extract_data',
+    sql=r"""use hr, select * from employees limit 10;"""
+)
